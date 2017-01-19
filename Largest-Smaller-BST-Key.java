8433722bdc4eadd79148a6d870497be4893511dc
MarcHines <mxh857@miami.edu>
2017-01-19 14:09:16 -0500

    Create Largest-Smaller-BST-Key.java

---
+++ Largest-Smaller-BST-Key.java
@@ -0,0 +1,24 @@
+/*
+Given a root of a binary search tree and a key x, find the largest key in the tree that is smaller than x.
+
+Example: if an in-order list of all keys in the tree is 
+{2, 3, 4, 7, 17, 19, 21, 35, 89} and x is 19,
+the biggest key that is smaller than x is 17.
+*/
+
+
+public static int findLargestSmallerThan(Node root, int k){
+       int result = Integer.MIN_VALUE;
+       
+       while(root != null){
+         if(root.value < k){
+           result = root.value;
+           root = root.rightChild;
+         }else{
+           root = root.leftChild;
+         }
+       
+       }
+       return result;
+   }
+    
