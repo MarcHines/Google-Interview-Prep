44b86fdba797297db4de22695c2758b7c828179e
MarcHines <mxh857@miami.edu>
2017-01-26 00:36:47 -0500

    Create Delete-Node-BST.java

---
+++ Delete-Node-BST.java
@@ -0,0 +1,96 @@
+/* 
+Delete a Node from a BST
+
+    Recursively find the node that has the same value as the key, while setting the left/right nodes equal to the returned subtree
+    Once the node is found, have to handle the below 4 cases
+
+    node doesn't have left or right - return null
+    node only has left subtree- return the left subtree
+    node only has right subtree- return the right subtree
+    node has both left and right - find the minimum value in the right subtree, set that value to the currently found node, then recursively delete the minimum value in the right subtree
+
+
+
+
+*/
+
+//Recursive
+public TreeNode deleteNode(TreeNode root, int key) {
+    if(root == null){
+        return null;
+    }
+    if(key < root.val){
+        root.left = deleteNode(root.left, key);
+    }else if(key > root.val){
+        root.right = deleteNode(root.right, key);
+    }else{
+        if(root.left == null){
+            return root.right;
+        }else if(root.right == null){
+            return root.left;
+        }
+        
+        TreeNode minNode = findMin(root.right);
+        root.val = minNode.val;
+        root.right = deleteNode(root.right, root.val);
+    }
+    return root;
+}
+
+private TreeNode findMin(TreeNode node){
+    while(node.left != null){
+        node = node.left;
+    }
+    return node;
+}
+
+
+
+
+
+
+
+//Iterative
+private TreeNode deleteRootNode(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        if (root.left == null) {
+            return root.right;
+        }
+        if (root.right == null) {
+            return root.left;
+        }
+        TreeNode next = root.right;
+        TreeNode pre = null;
+        for(; next.left != null; pre = next, next = next.left);
+        next.left = root.left;
+        if(root.right != next) {
+            pre.left = next.right;
+            next.right = root.right;
+        }
+        return next;
+    }
+    
+    public TreeNode deleteNode(TreeNode root, int key) {
+        TreeNode cur = root;
+        TreeNode pre = null;
+        while(cur != null && cur.val != key) {
+            pre = cur;
+            if (key < cur.val) {
+                cur = cur.left;
+            } else if (key > cur.val) {
+                cur = cur.right;
+            }
+        }
+        if (pre == null) {
+            return deleteRootNode(cur);
+        }
+        if (pre.left == cur) {
+            pre.left = deleteRootNode(cur);
+        } else {
+            pre.right = deleteRootNode(cur);
+        }
+        return root;
+    }
+
