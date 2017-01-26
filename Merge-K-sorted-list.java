33f62fc914f3ba7850f42b360bc1ce190bbdabb0
MarcHines <mxh857@miami.edu>
2017-01-26 10:19:20 -0500

    Create Merge-K-sorted-list.java

---
+++ Merge-K-sorted-list.java
@@ -0,0 +1,60 @@
+//Merge two sorted List. Merge K sorted lists
+
+
+public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        ListNode head=null;
+        ListNode former=null;
+        while (l1!=null&&l2!=null) {
+            if (l1.val>l2.val) {
+                if (former==null) former=l2; else former.next=l2;
+                if (head==null) head=former; else former=former.next;
+                l2=l2.next;
+            } else {
+                if (former==null) former=l1; else former.next=l1;
+                if (head==null) head=former; else former=former.next;
+                l1=l1.next;
+            }
+        }
+        if (l2!=null) l1=l2;
+        former.next=l1;
+        return head;
+        
+    }
+
+
+//K SORTED
+
+public ListNode mergeKLists(List<ListNode> lists) {
+        if (lists==null||lists.size()==0) return null;
+        
+        PriorityQueue<ListNode> queue= new PriorityQueue<ListNode>(lists.size(),new Comparator<ListNode>(){
+            @Override
+            public int compare(ListNode o1,ListNode o2){
+                if (o1.val<o2.val)
+                    return -1;
+                else if (o1.val==o2.val)
+                    return 0;
+                else 
+                    return 1;
+            }
+        });
+        
+        ListNode dummy = new ListNode(0);
+        ListNode tail=dummy;
+        
+        for (ListNode node:lists)
+            if (node!=null)
+                queue.add(node);
+            
+        while (!queue.isEmpty()){
+            tail.next=queue.poll();
+            tail=tail.next;
+            
+            if (tail.next!=null)
+                queue.add(tail.next);
+        }
+        return dummy.next;
+    }
