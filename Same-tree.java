7aa8545500a153f1b8913393f7179674a1473b61
MarcHines <mxh857@miami.edu>
2017-01-26 13:26:48 -0500

    Create Same-tree.java

---
+++ Same-tree.java
@@ -0,0 +1,9 @@
+/* Determine if two trees are identicle */
+
+public boolean isSameTree(TreeNode p, TreeNode q) {
+    if(p == null && q == null) return true;
+    if(p == null || q == null) return false;
+    if(p.val == q.val)
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    return false;
+}
