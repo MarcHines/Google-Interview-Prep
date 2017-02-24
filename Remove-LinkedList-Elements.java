d6d331f2a76e777ff4a0aea24a83b074308b2d4a
MarcHines <mxh857@miami.edu>
2017-02-24 10:32:12 -0500

    Create Remove-LinkedList-Elements.java

---
+++ Remove-LinkedList-Elements.java
@@ -0,0 +1,10 @@
+/*
+Remove all nodes from a Linked list that are equal to given value
+*/
+
+
+public ListNode removeElements(ListNode head, int val) {
+        if (head == null) return null;
+        head.next = removeElements(head.next, val);
+        return head.val == val ? head.next : head;
+}
