ded1791cb16e9587b97abf52d08d373d4500beaa
MarcHines <mxh857@miami.edu>
2017-04-06 11:58:15 -0400

    Create Bellmond-Ford.java

---
+++ Bellmond-Ford.java
@@ -0,0 +1,69 @@
+/*
+Following are the detailed steps.
+
+Input: Graph and a source vertex src
+Output: Shortest distance to all vertices from src. If there is a negative weight cycle, then shortest distances are not calculated, negative weight cycle is reported.
+
+1) This step initializes distances from source to all vertices as infinite and distance to source itself as 0. Create an array dist[] of size |V| with all values as infinite except dist[src] where src is source vertex.
+
+2) This step calculates shortest distances. Do following |V|-1 times where |V| is the number of vertices in given graph.
+…..a) Do following for each edge u-v
+………………If dist[v] > dist[u] + weight of edge uv, then update dist[v]
+………………….dist[v] = dist[u] + weight of edge uv
+
+3) This step reports if there is a negative weight cycle in graph. Do following for each edge u-v
+……If dist[v] > dist[u] + weight of edge uv, then “Graph contains negative weight cycle”
+The idea of step 3 is, step 2 guarantees shortest distances if graph doesn’t contain negative weight cycle. If we iterate through all edges one more time and get a shorter path for any vertex, then there is a negative weight cycle
+
+How does this work? Like other Dynamic Programming Problems, the algorithm calculate shortest paths in bottom-up manner. It first calculates the shortest distances for the shortest paths which have at-most one edge in the path. Then, it calculates shortest paths with at-nost 2 edges, and so on. After the ith iteration of outer loop, the shortest paths with at most i edges are calculated. There can be maximum |V| – 1 edges in any simple path, that is why the outer loop runs |v| – 1 times. The idea is, assuming that there is no negative weight cycle, if we have calculated shortest paths with at most i edges, then an iteration over all edges guarantees to give shortest path with at-most (i+1) edges (Proof is simple, you can refer this or MIT Video Lecture)
+
+
+
+
+*/
+
+
+
+
+void BellmanFord(Graph graph,int src)
+    {
+        int V = graph.V, E = graph.E;
+        int dist[] = new int[V];
+ 
+        // Step 1: Initialize distances from src to all other
+        // vertices as INFINITE
+        for (int i=0; i<V; ++i)
+            dist[i] = Integer.MAX_VALUE;
+        dist[src] = 0;
+ 
+        // Step 2: Relax all edges |V| - 1 times. A simple
+        // shortest path from src to any other vertex can
+        // have at-most |V| - 1 edges
+        for (int i=1; i<V; ++i)
+        {
+            for (int j=0; j<E; ++j)
+            {
+                int u = graph.edge[j].src;
+                int v = graph.edge[j].dest;
+                int weight = graph.edge[j].weight;
+                if (dist[u]!=Integer.MAX_VALUE &&
+                    dist[u]+weight<dist[v])
+                    dist[v]=dist[u]+weight;
+            }
+        }
+ 
+        // Step 3: check for negative-weight cycles.  The above
+        // step guarantees shortest distances if graph doesn't
+        // contain negative weight cycle. If we get a shorter
+        //  path, then there is a cycle.
+        for (int j=0; j<E; ++j)
+        {
+            int u = graph.edge[j].src;
+            int v = graph.edge[j].dest;
+            int weight = graph.edge[j].weight;
+            if (dist[u]!=Integer.MAX_VALUE &&
+                dist[u]+weight<dist[v])
+              System.out.println("Graph contains negative weight cycle");
+        }
+        printArr(dist, V);
+    }
