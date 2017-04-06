702059e95c8a75bfa9881203e288bcba44d7fc1d
MarcHines <mxh857@miami.edu>
2017-04-06 10:54:12 -0400

    Create Remove-Dups-from-sorted-array.java

---
+++ Remove-Dups-from-sorted-array.java
@@ -0,0 +1,12 @@
+public int removeDuplicates(int[] nums) {
+        if(nums==null || nums.length==0) {
+            return 0;
+        }
+        int j=0;
+        for(int i=0;i<nums.length;i++) {
+            if(nums[j]!=nums[i]){
+                nums[++j]=nums[i];
+            }
+        }
+        return j+1;
+    }
