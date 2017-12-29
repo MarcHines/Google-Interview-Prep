5c957df4efc980f63666684da28eb18b1ee42423
MarcHines <mxh857@miami.edu>
2017-12-28 20:02:42 -0500

    Update Implement-atoi.java

--- Implement-atoi.java
+++ Implement-atoi.java
@@ -6,15 +6,12 @@ eg. "109" Returns (int) 109
 */
 
 public static int atoi(String number){
-      HashSet<Integer> set = new HashSet<>();
-      for(int i = 0; i < 10; i++) set.add(i);
-
-      boolean isNegative = number.charAt(0) == '-' ? true : false;
+      boolean isNegative = number.charAt(0) == '-';
       int result = 0;
       
       for(int j = isNegative ? 1 : 0; j < number.length(); j++){
         char currentChar = number.charAt(j);
-        if(set.contains(currentChar - '0'))
+        if(Character.isDigit(currentChar))
           result =(result * 10) +  currentChar - '0';
     }
     
